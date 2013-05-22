568f130e5ae3aed53bf9e1e9f4dc50fa740f132b
Philipp Gayret <pgayret@gmail.com>
2013-05-23 00:05:59 +0200

    Array element working, any dimension! :)

--- main/hsf-array.js
+++ main/hsf-array.js
@@ -1,33 +1,36 @@
 module.directive('hsfArray', function factory($compile) {
 	return {
+		template:	'<div>'
+				+		'<a class="btn" ng-click="add_element()">Add</a>'
+				+		'<a class="btn" ng-click="remove_element()">Remove</a>'
+				+	'</div>',
 		restrict: 'A',
         scope: {
 			schema: '=',
 			model: '='
 		},
 		controller: function($scope, $element){
-			console.log("element:", $element);
 			console.log("array:", $scope.schema);
-
+			console.log("element:", $element);
+			var length = 0;
 			$scope.model = [];
 
 			$scope.add_element = function(){
-				var index = $scope.model.length;
-				var template = '<div hsf-element model="model[' + index + ']" schema="schema.element"/>';
+				// here we compile the template, with a static reference to the model element
+				var template = '<div hsf-element model="model[' + length + ']" schema="schema.kids"/>';
 				var compiled = $compile(template)($scope);
-				console.log("compiled:", compiled);
 				$element.append(compiled);
+				length++;
 			};
 
 			$scope.remove_element = function(){
+				var children = $element.children();
+				children[children.length - 1].remove();
+				length--;
 			};
-
-			// a test : add one element
-			$scope.add_element();
 
 		},
 		link: function postLink($scope, $element, $attrs){
-			$scope.model = {};
 		}
 	};
 });
--- main/hsf-element.js
+++ main/hsf-element.js
@@ -1,9 +1,19 @@
 module.directive('hsfElement', function factory($compile) {
 
-	var mapping = {
+	var mapping_named = {
 		'string': '<div hsf-input-string model="model[schema.name]" schema="schema"></div>',
+		'file': '<div hsf-input-file model="model[schema.name]" schema="schema"></div>',
+		'array': '<div hsf-array model="model[schema.name]" schema="schema"></div>',
 		'choice': '<div hsf-choice model="model" schema="schema"></div>',
 		'object': '<div hsf-object model="model[schema.name]" schema="schema"></div>'
+	};
+
+	var mapping_nameless = {
+		'string': '<div hsf-input-string model="model" schema="schema"></div>',
+		'file': '<div hsf-input-file model="model" schema="schema"></div>',
+		'array': '<div hsf-array model="model" schema="schema"></div>',
+		'choice': '<div hsf-choice model="model" schema="schema"></div>',
+		'object': '<div hsf-object model="model" schema="schema"></div>'
 	};
 
 	return {
@@ -18,6 +28,7 @@ module.directive('hsfElement', function factory($compile) {
 			var contents = $element.contents().remove();
 			var compiledContents;
 			return function($scope, $element){
+				var mapping = $scope.schema.name == undefined ? mapping_nameless : mapping_named;
 				compiled = $compile(mapping[$scope.schema.type])($scope);
 				$element.append(compiled);
 			};
